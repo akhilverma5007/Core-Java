05b2bff64e597465d567c8a2d444176a34f039f7
Akhil Verma <akhilverma@Akhils-MacBook-Pro.local>
2023-07-24 13:19:43 +0530

    New Changes on July 24th

--- SimpleInterest.java
+++
@@ -1,1 +0,0 @@
-// Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
--- TakeInputAsName.java
+++
@@ -1,9 +0,0 @@
-//Take name as input and print a greeting message for that particular name.
-import java.util.Scanner;
-public class TakeInputAsName {
-    public static void main(String[] args){
-       Scanner input = new Scanner(System.in);
-       String name = input.next();
-        System.out.println("Hi! Good Morning " + name);
-    }
-}
